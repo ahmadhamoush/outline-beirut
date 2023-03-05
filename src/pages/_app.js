b964560081f69b24b2b2b687e1ea1802dc0133cc
Ahmad Hamoush <hamoush@outlook.com>
2023-03-05 17:29:18 +0200

    added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,10 @@ import '@/styles/styles.css'
 import '@/styles/nav.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import "animate.css/animate.min.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps }
-    />
+    return <>
+    <Component {...pageProps } />
+    <Analytics /></>
 }
