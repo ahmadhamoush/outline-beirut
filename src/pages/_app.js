9823c74619f5085fc2ae33cdb8266567c69ea1d5
Ahmad Hamoush <hamoush@outlook.com>
2023-03-05 17:44:04 +0200

    added loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,16 @@ import '@/styles/nav.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import "animate.css/animate.min.css";
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const loader = document.getElementById('globalLoader');
+            if (loader)
+                loader.style.display = 'none';
+        }
+    }, []);
     return <>
     <Component {...pageProps } />
     <Analytics /></>
