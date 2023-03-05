670f7139726be75b84f5b0d8e90abad3f8346514
Ahmad Hamoush <hamoush@outlook.com>
2023-03-05 18:48:21 +0200

    fixed parallax

--- src/components/Location.js
+++ src/components/Location.js
@@ -137,15 +137,6 @@ function Location (){
       }
     return(
        <div className="mapSlide">
-        <div className='burgerContainer'>
-       <AnimationOnScroll className='syringe' animateIn='animate__fadeInDown'>
-        <Image alt='syringe' src='/syringe.webp' width={100} height={150}/>
-        </AnimationOnScroll>
-       <AnimationOnScroll animateIn="animate__fadeInUp">
-        <Image  alt="burger" className='burger' src='/burger.webp' width={140} height={250}/>
-        </AnimationOnScroll>
-    
-        </div>
          <div className="locationContainer">
         <div className="location">
             <AnimationOnScroll animateIn="animate__fadeInRight">
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -62,6 +62,15 @@ return(
               </div>
         </AnimationOnScroll>
     </div>
+    <div className='burgerContainer'>
+       <AnimationOnScroll className='syringe' animateIn='animate__fadeInDown'>
+        <Image alt='syringe' src='/syringe.webp' width={100} height={150}/>
+        </AnimationOnScroll>
+       <AnimationOnScroll animateIn="animate__fadeInUp">
+        <Image  alt="burger" className='burger' src='/burger.webp' width={140} height={250}/>
+        </AnimationOnScroll>
+    
+        </div>
       </Parallax>
   </div>
 
